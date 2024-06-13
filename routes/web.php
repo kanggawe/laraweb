dbacd4c7cafeeda8180cf5b95352bd89f0e17679
kanggawe <m.ibrahim.bajuri@gmail.com>
2024-06-13 10:24:07 +0700

    tambah file

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [\App\Http\Controllers\UserController::class, 'index']);
+Route::resource('user', \App\Http\Controllers\UserController::class);
 Route::get('/', function () {
     return view('welcome');
 });
